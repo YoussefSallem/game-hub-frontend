9298efd01dbb1759c6920ce47322eb8ef3af7c47
Youssef Salem <YousseffSallem@gmail.com>
2025-04-28 11:02:55 +0300

    initial commit

---
+++ src/app/filteration/filteration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filteration',
+  imports: [],
+  templateUrl: './filteration.component.html',
+  styleUrl: './filteration.component.css'
+})
+export class FilterationComponent {
+
+}
---
+++ src/app/order-by/order-by.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-by',
+  imports: [],
+  templateUrl: './order-by.component.html',
+  styleUrl: './order-by.component.css'
+})
+export class OrderByComponent {
+
+}
