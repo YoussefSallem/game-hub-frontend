3ee9aaf410e392a8ba1d87fc97322fdcc3bd7463
Youssef <youssefyaser2020@gmail.com>
2025-04-30 02:42:09 +0300

    Creating api-games services & environment

---
+++ src/app/services/api-games.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiGamesService {
+  constructor(private httpClient: HttpClient) {}
+  getAllGames() {
+    return this.httpClient.get(`${environment.baseUrl}/games`);
+  }
+}
