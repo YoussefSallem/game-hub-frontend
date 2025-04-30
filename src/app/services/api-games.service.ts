595e71692945fe91899aadf1ba3a799274db204a
Youssef <youssefyaser2020@gmail.com>
2025-04-30 22:45:53 +0300

    extract res.data from API response and fix type API response in getAllGames()

--- src/app/services/api-games.service.ts
+++ src/app/services/api-games.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiGamesService {
   constructor(private httpClient: HttpClient) {}
-  getAllGames() {
+  getAllGames():Observable<any> {
     return this.httpClient.get(`${environment.baseUrl}/games`);
   }
 }
