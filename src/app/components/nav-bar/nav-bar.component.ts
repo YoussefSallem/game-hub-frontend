9ea22259d70481ddb7432155192b70e524904b87
Mandour <mahmoudmandour200015@gmail.com>
2025-05-06 21:22:26 +0300

    replace login with logout when user is authenticated

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { DarkModeToggleComponent } from '../dark-mode-toggle/dark-mode-toggle.component';
+import { ApiLoginService } from '../../services/api-login.service';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterModule,DarkModeToggleComponent],
+  imports: [RouterLink, RouterModule, DarkModeToggleComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  constructor(
+    private _apiLoginService: ApiLoginService,
+    private router: Router
+  ) {}
+
   isMenuOpen: boolean = false;
+  isLoginIn: boolean = false;
+
+  ngOnInit(): void {
+    this.isLoginIn = this._apiLoginService.isLoggedIn();
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -17,5 +28,12 @@ export class NavBarComponent {
 
   closeMenu() {
     this.isMenuOpen = false;
+  }
+
+  logout() {
+    this._apiLoginService.logout();
+    this.isLoginIn = false;
+    this.closeMenu();
+    this.router.navigate(['/home']);
   }
 }
