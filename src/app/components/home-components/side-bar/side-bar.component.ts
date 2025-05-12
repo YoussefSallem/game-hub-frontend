1db1881f6b5f1a177f55eaa54ee70d68dc8c0576
KareemEhab <kareem8002010@gmail.com>
2025-05-12 23:27:11 +0300

    fix sidebar highlight on click

--- src/app/components/home-components/side-bar/side-bar.component.ts
+++ src/app/components/home-components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarService } from '../../../services/sidebar.service';
 import { RouterModule } from '@angular/router';
@@ -10,11 +10,12 @@ import { GenreService } from '../../../services/genre.service';
   imports: [CommonModule, RouterModule],
   templateUrl: './side-bar.component.html',
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
   @Input() isLoading: boolean = false;
   @Input() genres: any[] = [];
   @Input() selectedGenre: string | null = null;
   @Output() genreSelected = new EventEmitter<string | null>();
+
   activeButton: 'buy' | 'wishlist' | null = 'buy';
 
   constructor(
@@ -22,23 +23,33 @@ export class SideBarComponent {
     private genreService: GenreService
   ) {}
 
+  ngOnInit() {
+    this.genreService.selectedGenre$.subscribe((genre) => {
+      this.selectedGenre = genre;
+      if (genre) {
+        this.activeButton = null;
+      }
+    });
+  }
+
   onGenreClick(genreId: string) {
     this.genreService.setSelectedGenre(genreId);
     this.genreSelected.emit(genreId);
+    this.activeButton = null;
     this.sidebarService.close();
   }
 
   onBuyGamesClick() {
     this.genreService.setSelectedGenre(null);
     this.genreSelected.emit(null);
-    this.sidebarService.close();
     this.activeButton = 'buy';
+    this.sidebarService.close();
   }
 
   onWishlistClick() {
     this.genreService.setSelectedGenre(null);
     this.genreSelected.emit(null);
-    this.sidebarService.close();
     this.activeButton = 'wishlist';
+    this.sidebarService.close();
   }
 }
